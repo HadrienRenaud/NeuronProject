211642f9821cee05bdba7684c5d50c937b249326
HadrienRenaudLebret <hadrien.renaudlebret@gmail.com>
2017-01-12 18:13:15 +0100

    Holes working.

--- topology.cpp
+++ topology.cpp
@@ -3,92 +3,161 @@
 using namespace std;
 
 ToMatrix::ToMatrix(const int nbRow, const int nbCol)
-    : m_nbRow(nbRow), m_nbCol(nbCol) {}
-int ToMatrix::operator()(int row, int col) { return m_nbCol * row + col; }
-const int ToMatrix::getRow() const { return m_nbRow; }
-const int ToMatrix::getCol() const { return m_nbCol; }
+	: m_nbRow(nbRow), m_nbCol(nbCol) {
+}
+int ToMatrix::operator()(int row, int col) {
+	return m_nbCol * row + col;
+}
+const int ToMatrix::getRow() const {
+	return m_nbRow;
+}
+const int ToMatrix::getCol() const {
+	return m_nbCol;
+}
 
 bool ToMatrix::isValid(int row, int col) {
-  return row > -1 && row < getRow() && col > -1 && col < getCol();
+	return row > -1 && row < getRow() && col > -1 && col < getCol();
 }
 
 void print_matrix(int *matrix, ToMatrix t) {
-  std::cout << "Matrix : " << t.getRow() << " - " << t.getCol() << std::endl;
-  for (int i = 0; i < t.getRow(); i++) {
-    for (int j = 0; j < t.getCol(); j++) {
-      std::cout << matrix[t(i, j)] << " " << flush;
-    }
-    std::cout << std::endl;
-  }
+	std::cout << "Matrix : " << t.getRow() << " - " << t.getCol() << std::endl;
+	for (int i = 0; i < t.getRow(); i++) {
+		for (int j = 0; j < t.getCol(); j++) {
+			std::cout << matrix[t(i, j)] << " " << flush;
+		}
+		std::cout << std::endl;
+	}
 }
 
 void changeConnected(int row, int col, int *matrix, int *matAux, ToMatrix t) {
-  if (t.isValid(row + 1, col) && matAux[t(row + 1, col)] == 0 &&
-      matrix[t(row + 1, col)] == 1) {
-    matAux[t(row + 1, col)] = matAux[t(row, col)];
-    changeConnected(row + 1, col, matrix, matAux, t);
-  }
-  if (t.isValid(row - 1, col) && matAux[t(row - 1, col)] == 0 &&
-      matrix[t(row - 1, col)] == 1) {
-    matAux[t(row - 1, col)] = matAux[t(row, col)];
-    changeConnected(row - 1, col, matrix, matAux, t);
-  }
-  if (t.isValid(row, col + 1) && matAux[t(row, col + 1)] == 0 &&
-      matrix[t(row, col + 1)] == 1) {
-    matAux[t(row, col + 1)] = matAux[t(row, col)];
-    changeConnected(row, col + 1, matrix, matAux, t);
-  }
-  if (t.isValid(row, col - 1) && matAux[t(row, col - 1)] == 0 &&
-      matrix[t(row, col - 1)] == 1) {
-    matAux[t(row, col - 1)] = matAux[t(row, col)];
-    changeConnected(row, col - 1, matrix, matAux, t);
-  }
+	int comp = matrix[t(row, col)];
+	if (t.isValid(row + 1, col) && matAux[t(row + 1, col)] == 0 &&
+	    matrix[t(row + 1, col)] == comp) {
+		matAux[t(row + 1, col)] = matAux[t(row, col)];
+		changeConnected(row + 1, col, matrix, matAux, t);
+	}
+	if (t.isValid(row - 1, col) && matAux[t(row - 1, col)] == 0 &&
+	    matrix[t(row - 1, col)] == comp) {
+		matAux[t(row - 1, col)] = matAux[t(row, col)];
+		changeConnected(row - 1, col, matrix, matAux, t);
+	}
+	if (t.isValid(row, col + 1) && matAux[t(row, col + 1)] == 0 &&
+	    matrix[t(row, col + 1)] == comp) {
+		matAux[t(row, col + 1)] = matAux[t(row, col)];
+		changeConnected(row, col + 1, matrix, matAux, t);
+	}
+	if (t.isValid(row, col - 1) && matAux[t(row, col - 1)] == 0 &&
+	    matrix[t(row, col - 1)] == comp) {
+		matAux[t(row, col - 1)] = matAux[t(row, col)];
+		changeConnected(row, col - 1, matrix, matAux, t);
+	}
 }
 
 int nbConnectedComponent(double *image, const int taille) {
-  int compteur = 0;
-  int matrix[taille * taille];
-  int matAux[taille * taille];
-  ToMatrix t(taille,taille);
+	int compteur = 0;
+	int matrix[taille * taille];
+	int matAux[taille * taille];
+	ToMatrix t(taille,taille);
 
-  for (int i = 0; i < taille * taille; i++)
-  {
-    matAux[i] = 0;
+	for (int i = 0; i < taille * taille; i++)
+	{
+		matAux[i] = 0;
 
-    if (image[i] >= LIMIT_EMPTY_TOPOLOGY)
-      matrix[i] = 1;
-    else
-      matrix[i] = 0;
-  }
+		if (image[i] >= LIMIT_EMPTY_TOPOLOGY)
+			matrix[i] = 1;
+		else
+			matrix[i] = 0;
+	}
 
-  for (int i = 1; i < taille; i++) {
-    for (int j = 1; j < taille; j++) {
-      if (matrix[t(i, j)] == 1 and matAux[t(i, j)] == 0) {
-        compteur++;
-        matAux[t(i, j)] = compteur;
-        changeConnected(i, j, matrix, matAux, t);
-      }
-    }
-  }
+	for (int i = 1; i < taille; i++) {
+		for (int j = 1; j < taille; j++) {
+			if (matrix[t(i, j)] == 1 and matAux[t(i, j)] == 0) {
+				compteur++;
+				matAux[t(i, j)] = compteur;
+				changeConnected(i, j, matrix, matAux, t);
+			}
+		}
+	}
 
-  print_matrix(matAux, t);
-  std::cout << "Resultat: " << compteur << '\n';
+	// print_matrix(matAux, t);
+	std::cout << "Resultat: " << compteur << '\n';
 
-  return compteur;
+	return compteur;
 }
 
 
 
 int nbConnectedComponentMatrix(double image[TAILLE][TAILLE])
 {
-    double line[TAILLE*TAILLE];
+	double line[TAILLE*TAILLE];
 
-    for (int j = 0; j < TAILLE; j++)
-    {
-        for (int i = 0; i < TAILLE; i++)
-            line[j*TAILLE + i] = image[j][i];
+	for (int j = 0; j < TAILLE; j++)
+	{
+		for (int i = 0; i < TAILLE; i++)
+			line[j*TAILLE + i] = image[j][i];
 
-    }
+	}
 
-    return nbConnectedComponent(line, TAILLE);
+	return nbHoles(line, TAILLE);
+}
+
+int nbHoles(double *image, const int taille) {
+	int nbSpaces = 0;
+	int nbHoles = 0;
+	int matrix[taille * taille];
+	int matAux[taille * taille];
+	ToMatrix t(taille,taille);
+
+	// matAux initialisation, matrix format
+	for (int i = 0; i < taille * taille; i++)
+	{
+		matAux[i] = 0;
+
+		if (image[i] >= LIMIT_EMPTY_TOPOLOGY)
+			matrix[i] = 1;
+		else
+			matrix[i] = 0;
+	}
+
+	// Border initialisation
+	for (int i=0; i < taille; i++)
+	{
+		if (matrix[t(0, i)] == 0) {
+			matAux[t(0, i)] = -1;
+			changeConnected(0, i, matrix, matAux, t);
+		}
+		if (matrix[t(taille - 1, i)] == 0) {
+			matAux[t(taille - 1, i)] = -1;
+			changeConnected(taille - 1, i, matrix, matAux, t);
+		}
+		if (matrix[t(i, 0)] == 0 && matAux[t(i, 0)] == 0) {
+			matAux[t(i, 0)] = -1;
+			changeConnected(i, 0, matrix, matAux, t);
+		}
+		if (matrix[t(i, taille - 1)] == 0 && matAux[t(i, taille - 1)]) {
+			matAux[t(i, taille -1)] = -1;
+			changeConnected(i, taille - 1, matrix, matAux, t);
+		}
+	}
+
+	// print_matrix(matAux,t);
+
+	// Count nbHoles
+	for (int i = 1; i < taille; i++) {
+		for (int j = 1; j < taille; j++) {
+			if (matAux[t(i, j)] == 0) {
+				nbSpaces++;
+				if (matrix[t(i, j)] == 0) {
+					nbHoles++;
+				}
+				matAux[t(i, j)] = nbSpaces;
+				changeConnected(i, j, matrix, matAux, t);
+			}
+		}
+	}
+
+	// print_matrix(matAux, t);
+	std::cout << "Résultat :" << nbHoles << '\n';
+
+	return nbHoles;
 }
--- topology.h
+++ topology.h
@@ -29,6 +29,13 @@ void changeConnected(int row, int col, int *matrix, int *matAux, ToMatrix t);
  */
 int nbConnectedComponent(double *image, const int taille = TOPOLOGY_INPUT_SIZE);
 
+//! Determine the number of holes
+/*! In the image passed as component
+    \param matrix : image
+    \return nombre int number of holes
+ */
+int nbHoles(double *image, const int taille = TOPOLOGY_INPUT_SIZE);
+
 
 //! Transforme une matrice correspondant à une image filtrée en une ligne dont on calcule le nombre de composantes connexes.
 int nbConnectedComponentMatrix(double image[TAILLE][TAILLE]);
